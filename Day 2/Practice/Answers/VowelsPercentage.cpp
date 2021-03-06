9b34a49c8382479dd80bc6e68521f6e22e42a323
manikanta2901 <manikanta2901@gmail.com>
2021-03-06 16:26:00 +0530

    Day 2 problems are added

---
+++ Day 2/Practice/Answers/VowelsPercentage.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name;
+    cin >> name;
+    int vowelCount = 0;
+    for(int i = 0; i < name.length(); i++){
+        if(name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i] == 'u'|| name[i] == 'A' || name[i] == 'E' || name[i] == 'I' || name[i] == 'O' || name[i] == 'U'){
+            vowelCount++;
+        }
+    }
+    float answer = ((float)(vowelCount) / name.size()) *100;
+    cout << fixed << setprecision(4) << answer << endl;
+    return 0;
+}
